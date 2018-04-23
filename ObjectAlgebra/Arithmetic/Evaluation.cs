4041a430d8810a3ef7384d1135ac56b96ea7f042
Marco Buchholz <buchholz@hypros.de>
2018-04-23 17:20:50 +0200

    Move ObjectAlgebra definition in own files

---
+++ ObjectAlgebra/Arithmetic/Evaluation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using ObjectAlgebra.DynamicProxy;
+
+namespace ObjectAlgebra.Arithmetic
+{
+    // Initial object algebra interface for expressions: integers and addition
+    public interface IExpAlg<E>
+    {
+        E Lit(int x);
+        E Add(E e1, E e2);
+    }
+
+    // ----------------------------------------------------------------------
+    //       An object algebra implementing that interface (evaluation)
+    // ----------------------------------------------------------------------
+
+    // The evaluation interface
+    public interface IEval
+    {
+        int Eval();
+    }
+
+    // The object algebra
+    class EvalExpAlg : IExpAlg<IEval>
+    {
+        public IEval Lit(int x)
+        {
+            Func<int> f = () => x;
+            return DelegateWrapper.WrapAs<IEval>(f);
+        }
+
+        public IEval Add(IEval e1, IEval e2)
+        {
+            Func<int> f = () => e1.Eval() + e2.Eval();
+            return DelegateWrapper.WrapAs<IEval>(f);
+        }
+    }
+}
