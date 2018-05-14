75f37a9eb7bbf11c1a4f9b085ddf688b47006c6c
Marco Buchholz <buchholz@hypros.de>
2018-05-14 22:34:07 +0200

    Implemented Pretty Print

--- ObjectAlgebra/Ltl/PrettyPrint.cs
+++ ObjectAlgebra/Ltl/PrettyPrint.cs
@@ -1,45 +1,56 @@
+using System;
+using ObjectAlgebra.DynamicProxy;
+
 namespace ObjectAlgebra.Ltl
 {
     public class PrettyPrint : ILtlExtension<IPrettyPrint>
     {
         public IPrettyPrint Conjunction(IPrettyPrint left, IPrettyPrint right)
         {
-            throw new System.NotImplementedException();
+            Func<string> f = () => left.Print() + " ∧ " + right.Print();
+            return DelegateWrapper.WrapAs<IPrettyPrint>(f);
         }
 
         public IPrettyPrint Disjunction(IPrettyPrint left, IPrettyPrint right)
         {
-            throw new System.NotImplementedException();
+            Func<string> f = () => left.Print() + " ∨ " + right.Print();
+            return DelegateWrapper.WrapAs<IPrettyPrint>(f);
         }
 
         public IPrettyPrint Finally(IPrettyPrint child)
         {
-            throw new System.NotImplementedException();
+            Func<string> f = () => "F" + child.Print();
+            return DelegateWrapper.WrapAs<IPrettyPrint>(f);
         }
 
         public IPrettyPrint Negation(IPrettyPrint child)
         {
-            throw new System.NotImplementedException();
+            Func<string> f = () => "!" + child.Print();
+            return DelegateWrapper.WrapAs<IPrettyPrint>(f);
         }
 
         public IPrettyPrint Next(IPrettyPrint child)
         {
-            throw new System.NotImplementedException();
+            Func<string> f = () => "X" + child.Print();
+            return DelegateWrapper.WrapAs<IPrettyPrint>(f);
         }
 
         public IPrettyPrint Proposition(bool val)
         {
-            throw new System.NotImplementedException();
+            Func<string> f = () => val.ToString();
+            return DelegateWrapper.WrapAs<IPrettyPrint>(f);
         }
 
         public IPrettyPrint Until(IPrettyPrint left, IPrettyPrint right)
         {
-            throw new System.NotImplementedException();
+            Func<string> f = () => left.Print() + " U " + right.Print();
+            return DelegateWrapper.WrapAs<IPrettyPrint>(f);
         }
 
         public IPrettyPrint Variable(string name)
         {
-            throw new System.NotImplementedException();
+            Func<string> f = () => name;
+            return DelegateWrapper.WrapAs<IPrettyPrint>(f);
         }
     }
 }
