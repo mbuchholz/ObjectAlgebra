dc071bbec51f1ab8584ba427d8ac1fa7b1db99d0
Max Golubew <golubew@hypros.de>
2018-05-22 07:22:25 +0200

    Created project for unit testing.

---
+++ ObjectAlgebra.Tests/BasicFunctionality.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace ObjectAlgebra.Tests {
+
+    public class BasicFunctionality {
+
+        public BasicFunctionality() {
+        }
+
+        [Fact]
+        public void FirstTest() {
+            throw new NotImplementedException("Please create a test first.");
+        }
+    }
+}
